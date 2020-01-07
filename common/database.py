b42ecc141aaa1a8cb47d9ba16c406f61afa28d69
Sivananda Panda <sivananda.panda@gmail.com>
2020-01-07 18:58:21 +0530

    Update database.py

--- common/database.py
+++ common/database.py
@@ -4,12 +4,12 @@ import pymongo
 
 
 class Database(object):
-    URI = os.environ.get("MONGOLAB_URI")
-    DATABASE = None
+    #URI = os.environ.get("MONGOLAB_URI")
+    #DATABASE = None
 
     @staticmethod
     def initialize():
-        client = pymongo.MongoClient(Database.URI)
+        client = pymongo.MongoClient("mongodb://Test:Test123@ds259528.mlab.com:59528/heroku_zptt3q6d")
         Database.DATABASE = client.get_default_datbase()
 
     @staticmethod
